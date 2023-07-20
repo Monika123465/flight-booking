ae53df5d7b72464d1a6f37f0b7d706141ba5dbfb
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-07-20 16:56:59 +0530

    footer done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,69 +1,57 @@
 import React from 'react'
-import flightlogo from "../assets/flightlogo.jpg"
-import {AiFillFacebook,AiFillTwitterSquare,AiFillYoutube,AiFillGoogleCircle} from "react-icons/ai"
+import facebook from "../assets/facebook.png"
+import twitter from "../assets/twitter.png"
+import youtube from "../assets/youtube.png"
+import instagram from "../assets/instagram.jpeg"
+
 
 const Footer = () => {
   return (
-    <div class="bg-neutral-300 leading-10	">
-        <div class='w-2/5 border-2 border-rose-600 m-auto text-center '>
-           <div class="mt-12 text-xxl"><p>Subscribe Newsletter & get Latest News</p></div>
-           <div>
-            <input placeholder='Enter you email address'  type='text'/>
-           <button>Subscribe</button>
-           </div>
-        </div>
+    <div class="bg-neutral-300 leading-10 mt-12	">
 
+      <div class='w-2/5 m-auto text-center border-2 border-transparent'>
+        <div class="w-5/6  mt-24 "><p>Subscribe Newsletter & get Latest News</p></div>
+        <input class='w-4/5 h-8 rounded-md ' placeholder='Enter you email address' type='text' />
+        <button class=' w-24 h-12 rounded-md  border-2 border-cyan-600 ml-8 '>Subscribe</button>
+      </div>
+
+
+      <div class=' ml-48 grid grid-cols-4 gap-4 mt-24 leading-normal '>
+        <div class=" w-full  ml-12  ">
+          <p>Your mind should be stronger  than your feelings.fly !</p>
+          <div class="flex w-4/6  h-8"><img src={facebook} alt='err' /> <img src={twitter} alt='err' /> <img src={youtube} alt='err' /> <img src={instagram} alt='err' /></div>
+        </div>
+        <div class='ml-24'>
+          <h1 class='mb-8'>Information</h1>
+          <p>Home</p>
+          <p>Explore</p>
+          <p>Flight Status</p>
+          <p>Travel</p>
+          <p>Check-in</p>
+          <p>Manage your booking</p>
+        </div>
         <div>
-            <div class="">
-             
-             <div> <p>Your mind should be stronger  than your feelings.fly !</p></div>
-             <div><AiFillFacebook/> <AiFillTwitterSquare/> <AiFillYoutube/> <AiFillGoogleCircle/></div>
-                           
-            </div>
-
-              <div>
-                <div>
-                  <h1>Information</h1>
-                </div>
-                <div>
-                  <p>Home</p>
-                  <p>Explore</p>
-                  <p>Flight Status</p>
-                  <p>Travel</p>
-                  <p>Check-in</p>
-                  <p>Manage your booking</p>
-                </div>
-              </div>
-
-              <div>
-              <div>
-                  <h1>Quick Guide</h1>
-                </div>
-                <div>
-                  <p>FAQ</p>
-                  <p>How to</p>
-                  <p>Features</p>
-                  <p>Baggage</p>
-                  <p>Route Map</p>
-                  <p>Our communities</p>
-                </div>
-              </div>
-
-              <div>
-              <div>
-                  <h1>Information</h1>
-                </div>
-                <div>
-                  <p>Chauffuer</p>
-                  <p>Our partners</p>
-                  <p>Destination</p>
-                  <p>Careers</p>
-                  <p>Transportation</p>
-                  <p>Programme Rules</p>
-                </div>
-              </div>
+          <h1 class='mb-8'>Quick Guide</h1>
+          <p>FAQ</p>
+          <p>How to</p>
+          <p>Features</p>
+          <p>Baggage</p>
+          <p>Route Map</p>
+          <p>Our communities</p>
         </div>
-        
+        <div class='mb-24'>
+          <h1 class='mb-8'>Information</h1>
+          <p>Chauffuer</p>
+          <p>Our partners</p>
+          <p>Destination</p>
+          <p>Careers</p>
+          <p>Transportation</p>
+          <p>Programme Rules</p>
+        </div>
+
+
+      </div>
+
     </div>
   )
 }
