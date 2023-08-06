0d13ffbbc01548be09f94a6d18a46e8a635d7495
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-08-06 09:23:26 +0530

    css to travell

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Components/Navbar.js';
 import Footer from './Components/Footer';
 import Home from './Pages/Home';
+import Travell from './Pages/Travell.js'
 import { BrowserRouter as Router} from 'react-router-dom';
 
 
@@ -12,6 +13,7 @@ function App() {
       <Router>
        <Navbar/>
        <Home/>
+       <Travell/>
       <Footer/>
    </Router>
     
---
+++ src/Pages/Travell.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Travell = () => {
+  return (
+    <>
+    <div class="w-4/6  h-48 m-auto text-center mt-32">
+      <h1 class='text-stone-400 break-words mt-4'>T R A V E L   S U P P O R T</h1>
+      <h3 class='text-4xl mt-4'>Plan your travel with confidence</h3>
+      <p class='text-stone-400 mt-4'>Find help with booking and travel plans. See what to expect along the journey!</p>
+    </div>
+
+    <div>
+      <div>
+        <div>
+        <h1>Travel requirements for Dubai</h1>
+        <p>
+          Find help with booking and travel plans.see what to expect along the journey to your favourite destinations!
+        </p>
+        </div>
+
+        <div>
+        <h1>Chauffeur services at your arrival</h1>
+        <p>
+          Find help with booking and travel plans.see what to expect along the journey to your favourite destinations!
+        </p>
+        </div>
+        <div>
+        <h1>Multi-risk travel insurance</h1>
+        <p>
+          Find help with booking and travel plans.see what to expect along the journey to your favourite destinations!
+        </p>
+        </div>
+
+      </div>
+       <div></div>
+    </div>
+
+    </>
+  )
+}
+
+export default Travell
