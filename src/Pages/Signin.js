93a1f3dc9cb732c9c266f5e9bee410c918872bcb
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-09-10 23:12:13 +0530

    add css to login

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,44 +1,44 @@
 import React from 'react'
-import signin from "../assets/sign.jpg"
+import signin from "../assets/signinup.jpg"
 import google from "../assets/google.png"
 
 
 
 const Signin = () => {
    return (
-    <>
-    <div className='w-4/5 border-4  m-auto flex gap-48 mt-48 mb-48'>
-    <div className='w-2/5 h-48 mt-12 mb-12'>
-    <img src={signin} alt='err' />
+    <div >
+      <div className='bg-cyan-600 '>
+    <div className='w-3/5  gap-24  m-auto flex pt-48    shadow-lg'>
+    <div className=' mt-12 ml-6'>
+    <img className='' src={signin} alt='err' />
     </div>
-    <div className=' mt-12 w-4/5 mb-12 text-2xl'>
+    <div className=' w-4/6 mt-24  mb-12 text-2xl mr-6 '>
       <h1 className='text-5xl mb-6'>Welcome Onboard</h1>
-      <div className='flex gap-2 h-12 text-center border-4 w-96 items-center mb-2 '>
-        <img className='w-6 h-6 ml-4' src={google} alt='err' />
+      <div className='flex gap-2 h-6 text-center  w-96 items-center mb-12 '>
+        <img className='w-6 h-6 ' src={google} alt='err' />
         <p>Sign up with your google</p>
       </div>
-      <div className='w-96 border-4 mb-6 '>
-        <p>or,sign up with your email</p>
-      </div>
+      
+      
 
-      <div className='w-5/6 border-4 mt-12 mb-2'>
+      <div className='  mt-6 mb-2'>
         <p>Your name*</p>
-        <input type='text' placeholder='Enter your name' className='border-4 w-5/6 mb-4' />
+        <input type='text' placeholder='Enter your name' className='border-2 w-96 mb-4' />
 
         <p>Email address*</p>
-        <input type='text' placeholder='Enter your email' className='border-4 w-5/6 mb-4' />
+        <input type='text' placeholder='Enter your email' className='border-2 w-96 mb-4' />
 
         <p>Password*</p>
-        <input type='password' placeholder='Enter your password' className='border-4 w-5/6 mb-4' />
+        <input type='password' placeholder='Enter your password' className='border-2 w-96 mb-4' />
       </div>
-      <div><p>Agree to terms & conditions</p></div>
-     <div> <button>SignUp</button></div>
-     <div><p>Already have an  account ?</p><p>Sign in now</p></div>
+      <div className=' text-xl'><p>Agree to terms & conditions</p></div>
+     <div className='border-2 border-cyan-600 w-96 h-12 text-center mb-6 mt-6'> <button >SignUp</button></div>
+     <div className=' flex text-xl gap-2 '><p>Already have an  account ?</p ><button className='text-cyan-600'>Sign in now</button></div>
     </div>
     
     </div>
-    
-    </>
+    </div>
+    </div>
   )
 }
 
